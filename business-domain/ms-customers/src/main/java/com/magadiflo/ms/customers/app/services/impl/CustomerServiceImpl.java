81f228fe060cf93dc577d4af8ed025ab7dd9f29a
Martín <magadiflo@gmail.com>
2023-04-18 17:32:39 -0500

    WebFlux - Agregar dependencias y configuración básica de un cliente REST

--- business-domain/ms-customers/src/main/java/com/magadiflo/ms/customers/app/services/impl/CustomerServiceImpl.java
+++ business-domain/ms-customers/src/main/java/com/magadiflo/ms/customers/app/services/impl/CustomerServiceImpl.java
@@ -3,18 +3,41 @@ package com.magadiflo.ms.customers.app.services.impl;
 import com.magadiflo.ms.customers.app.entities.Customer;
 import com.magadiflo.ms.customers.app.repositories.ICustomerRepository;
 import com.magadiflo.ms.customers.app.services.ICustomerService;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.epoll.EpollChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @AllArgsConstructor
 @Service
 public class CustomerServiceImpl implements ICustomerService {
 
     private final ICustomerRepository customerRepository;
+    private final WebClient.Builder webClientBuilder;
+    private final HttpClient client = HttpClient.create()
+            // Connection Timeout: is a period within which a connection between a client and server must be established
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+            .option(ChannelOption.SO_KEEPALIVE, true)
+            .option(EpollChannelOption.TCP_KEEPIDLE, 300)
+            .option(EpollChannelOption.TCP_KEEPINTVL, 60)
+            // Response Timeout: The maximun time we wait to receive a response after sending a request
+            .responseTimeout(Duration.ofSeconds(1))
+            // Read an write timeout: a read timeout occurs when no data was read within a certain period of time,
+            // while the write timeout when a write operation cannot finish at a specific time
+            .doOnConnected(connection -> {
+                connection.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+                connection.addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+            });
 
     @Override
     @Transactional(readOnly = true)
