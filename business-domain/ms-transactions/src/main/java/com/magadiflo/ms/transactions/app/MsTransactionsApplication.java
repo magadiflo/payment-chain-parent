4479aa65e881fd43a8cebe97f3a9dad2a5d89972
Martín <magadiflo@gmail.com>
2023-04-19 10:32:44 -0500

    Agregando nuevo microservicio: ms-transactions

---
+++ business-domain/ms-transactions/src/main/java/com/magadiflo/ms/transactions/app/MsTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.magadiflo.ms.transactions.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTransactionsApplication.class, args);
+	}
+
+}
---
+++ business-domain/ms-transactions/src/test/java/com/magadiflo/ms/transactions/app/MsTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magadiflo.ms.transactions.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
