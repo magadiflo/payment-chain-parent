27d485e9b14d078d12e6b467ff6084e560aab1e5
Martín <magadiflo@gmail.com>
2023-04-20 11:09:43 -0500

    Administración de microservicios monitorización

--- infrastructure-domain/ms-admin-server/src/main/java/com/magadiflo/ms/admin/server/app/MsAdminServerApplication.java
+++ infrastructure-domain/ms-admin-server/src/main/java/com/magadiflo/ms/admin/server/app/MsAdminServerApplication.java
@@ -1,13 +1,29 @@
 package com.magadiflo.ms.admin.server.app;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableEurekaClient
+@EnableAdminServer
 @SpringBootApplication
 public class MsAdminServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsAdminServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MsAdminServerApplication.class, args);
+    }
+
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()
+                    .and().csrf().disable();
+        }
+    }
 
 }
