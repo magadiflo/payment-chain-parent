03bb231cc7e13929f563b0e08ba9123cf8be938d
Martín <magadiflo@gmail.com>
2023-04-20 10:23:07 -0500

    Agregando microservicio ms-admin-server

---
+++ infrastructure-domain/ms-admin-server/src/main/java/com/magadiflo/ms/admin/server/app/MsAdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.magadiflo.ms.admin.server.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAdminServerApplication.class, args);
+	}
+
+}
---
+++ infrastructure-domain/ms-admin-server/src/test/java/com/magadiflo/ms/admin/server/app/MsAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magadiflo.ms.admin.server.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
